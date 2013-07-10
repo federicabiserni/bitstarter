ea14996a2a402f9bd783ce73d7eddbe2f172d83d
Federica Biserni <federicabiserni@gmail.com>
2013-07-10 13:41:14 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-   response.send(fs.readFileSync('index.html','utf-8'));
+   var strIndex=(fs.readFileSync('index.html','utf-8');
+   response.send(strIndex);
 });
 
 var port = process.env.PORT || 5000;
